7db60df439168bbd0229e40ddafefa25c84b9ae6
zeng <573908667@qq.com>
2020-11-21 10:38:01 +0800

    'core'

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -10,35 +10,42 @@ namespace Uupt\core;
 
 class Core
 {
-    protected $app_id = 'ccba8bd4a2d54a2fb6df97e87979f303';
-    protected $app_key = '2815a7a1f8e3405d81fd6263683ec4e7';
-    protected $open_id = '910a0dfd12bb4bc0acec147bcb1ae246';
-    protected $url = 'http://openapi.test.uupt.com/v2_0/';
+    protected $url    = 'http://openapi.test.uupt.com/v2_0/';
+    protected $config = [
+        'app_id'  => 'ccba8bd4a2d54a2fb6df97e87979f303',
+        'app_key' => '2815a7a1f8e3405d81fd6263683ec4e7',
+        'open_id' => '910a0dfd12bb4bc0acec147bcb1ae246',
 
-    public function __construct(array $config,$is_test = false)
+    ];
+
+    public function __construct(array $config, $is_test = false)
     {
         if($is_test){
             if(!array_key_exists('app_id', $config) || empty($config['app_id'])){
                 throw new \Exception('miss config: app_id');
             }
+
             if(!array_key_exists('app_key', $config) || empty($config['app_key'])){
                 throw new \Exception('miss config: app_key');
             }
 
-            $this->app_id  = $config['app_id'];
-            $this->app_key = $config['app_key'];
-            $this->url     = 'https://openapi.uupt.com/v2_0';
+            if(!array_key_exists('open_id', $config) || empty($config['open_id'])){
+                throw new \Exception('miss config: open_id');
+            }
+
+            $this->config = $config;
+            $this->url    = 'https://openapi.uupt.com/v2_0';
         }
     }
 
     public function getAppId()
     {
-        return $this->app_id;
+        return $this->config['app_id'];
     }
 
     public function getAppKey()
     {
-        return $this->app_key;
+        return $this->config['app_key'];
     }
 
     public function getUrl()
@@ -48,7 +55,7 @@ class Core
 
     public function getOpenId()
     {
-        return $this->open_id;
+        return $this->config['open_id'];
     }
 
 }
